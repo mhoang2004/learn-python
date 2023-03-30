da19f5d2a1eb74cac7a1fb2d0dd4e38da876bd51
hoang95633 <leminhoang2004@gmail.com>
2023-03-30 11:52:20 +0700

    add

--- a.py
+++ a.py
@@ -1,6 +1,10 @@
+import random
 myList = [1, 'a', 4.5555, False]
 myInt = list(range(0, 9))
 colors = ['red', 'yellow', 'blue', 'green', 'grey']
 
 for color in colors:
     print(color)
+
+my_score = random.randint(0, 6)
+print(my_score)
