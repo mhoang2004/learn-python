f5284fdc532ef168d65b3723a071d7b52ff5bdc4
hoang95633 <leminhoang2004@gmail.com>
2023-04-20 21:49:51 +0700

    more things

--- a.py
+++ a.py
@@ -1,10 +1,58 @@
-import random
-myList = [1, 'a', 4.5555, False]
-myInt = list(range(0, 9))
-colors = ['red', 'yellow', 'blue', 'green', 'grey']
+# import random
+# myList = [1, 'a', 4.5555, False]
+# myInt = list(range(0, 9))
+# colors = ['red', 'yellow', 'blue', 'green', 'grey', 'red']
+# nums = [1, 3, 4, 6, 5, 5, 6, 7, 1, 2]
+# colors.append("violet")
 
-for color in colors:
-    print(color)
+# # i = 0
+# # while i < len(colors):
+# #     print(colors[i])
+# #     i += 1
+# names = ["Le", "Adam", "Hannah"]
+# output = ", ".join(names)
+# print(output)
 
-my_score = random.randint(0, 6)
-print(my_score)
+# from random import random
+
+# def flip_coin():
+#     r = random()  # from 0.00 to 0.99
+#     if r > 0.5:
+#         return "Heads"
+#     else:
+#         return "Tails"
+
+# print(flip_coin())
+
+# nums = [0, 1, 2, 3, 4, 5, 6, 7]
+# names = ['hoang', 'hien', 'long', 'khoa', 'ly']
+# even = filter(lambda x: x % 2 == 0, nums)
+# friend = map(lambda x: f"my friend is {x}",
+#              filter(lambda x: len(x) < 5, names))
+# print(list(friend))
+
+# for i in even:
+#     print(i)
+
+# nums1 = [1, 5, 3]
+# nums2 = [4, 2, 6]
+# names = ["Thomas", "Leo", "Jack"]
+
+# avg_grade = dict(
+#     zip(
+#         names,
+#         map(
+#             lambda pair: (pair[0] + pair[1]) / 2,
+#             zip(nums1, nums2)
+#         )
+#     )
+# )
+
+# print(avg_grade)
+
+def interleave(string1, string2):
+    res = ""
+    []
+
+
+print(interleave('aaa', 'zzz'))
